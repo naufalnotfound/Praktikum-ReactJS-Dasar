a0b77e9e4da4ff2f3d2798e93ee76ddc9ea5c0ca
naufalnotfound <firdausnaufal6@gmail.com>
2022-05-18 16:40:01 +0700

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+                    <p className='location-header'></p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h3 className='text-dark'>Klik pada salah satu menu di pojok atas!</h3>
+            </div>
+                
+    );
+};
+
+export default Home;
